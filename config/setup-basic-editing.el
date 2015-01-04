42dae1dfc3924ed0d9fc4bca1b5f0b1a4441477c
Bengt-Ove Holländer <Bengt.Ove.Hollaender@gmail.com>
2015-01-04 17:42:36 +0100

    Swap keybindings for newline and indent in prog mode

--- config/setup-basic-editing.el
+++ config/setup-basic-editing.el
@@ -1,3 +1,22 @@
 (setq-default fill-column 80)
 
+;; Sentence ends with period and one space
+(setq sentence-end-double-space nil)
+
+;; Swap the key-bindings of RET and C-j in prog-mode if they have the default
+;; bindings (newline and newline-and-indent).
+(defun swap-bindings (key1 key2)
+  "Swaps the local bindings of key1 and key2."
+  (let ((kb1 (key-binding key1))
+	(kb2 (key-binding key2)))
+    (local-set-key key1 kb2)
+    (local-set-key key2 kb1)))
+
+(add-hook 'prog-mode-hook
+	  (lambda ()
+	    (let ((kbd_RET (kbd "RET")) (kbd_Cj (kbd "C-j")))
+	      (if (equal '(newline newline-and-indent)
+			 (list (key-binding kbd_RET) (key-binding kbd_Cj)))
+		  (swap-bindings kbd_RET kbd_Cj)))))
+
 (provide 'setup-basic-editing)
