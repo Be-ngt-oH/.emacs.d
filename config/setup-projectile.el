c786fd7a357867401bcc5b765ccdd8ada853d7bc
Bengt-Ove Holländer <Bengt.Ove.Hollaender@gmail.com>
2015-01-07 23:50:23 +0100

    Define keybinding in mode instead of globally

--- config/setup-projectile.el
+++ config/setup-projectile.el
@@ -1,6 +1,6 @@
 (require 'projectile)
 (projectile-global-mode)
 
-(global-set-key (kbd "C-x f") 'projectile-find-file)
+(define-key projectile-mode-map (kbd "C-x f") 'projectile-find-file)
 
 (provide 'setup-projectile)
