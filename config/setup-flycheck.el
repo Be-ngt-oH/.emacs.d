b6c558ca443e1636100421bdc9a82ad32cc3a769
Bengt-Ove Holländer <Bengt.Ove.Hollaender@gmail.com>
2015-01-25 01:48:06 +0100

    Use flycheck in js2-mode

--- config/setup-flycheck.el
+++ config/setup-flycheck.el
@@ -6,8 +6,7 @@
   '(custom-set-variables
     '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
 
-;; Maybe change to 'lines later on
-(setq flycheck-highlighting-mode nil)
+(setq flycheck-highlighting-mode 'symbols)
 
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 (setq flycheck-emacs-lisp-load-path load-path)
--- config/setup-js2-mode.el
+++ config/setup-js2-mode.el
@@ -12,4 +12,8 @@
 
 (define-key js2-mode-map (kbd "C-M-h") 'js2-mark-defun)
 
+;; Disable js2 error highlighting - Flycheck seems to be much better
+(setq js2-mode-show-parse-errors nil
+      js2-mode-show-strict-warnings nil)
+
 (provide 'setup-js2-mode)
