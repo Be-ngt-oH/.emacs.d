a9ae5931b829721121289be5c2a40bdf6ace6f26
Bengt-Ove Holländer <Bengt.Ove.Hollaender@gmail.com>
2015-11-05 17:25:08 +0100

    Fix path for globally installed utilities on OS X

--- config/setup-osx-specifics.el
+++ config/setup-osx-specifics.el
@@ -2,4 +2,7 @@
       mac-command-modifier 'meta
       x-select-enable-clipboard t)
 
+;; Fix path for globally installed utilities
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
 (provide 'setup-osx-specifics)
