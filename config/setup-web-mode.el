07ab654019f99a82eb26d93d8ce8b16d5731c3f0
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2018-10-24 13:34:21 +0100

    Set default indent for web mode

--- config/setup-web-mode.el
+++ config/setup-web-mode.el
@@ -5,5 +5,8 @@
 (add-to-list 'web-mode-comment-formats '("javascript" . "//"))
 
 (setq-default web-mode-enable-auto-indentation nil)
+(setq-default web-mode-markup-indent-offset 2)
+(setq-default web-mode-code-indent-offset 2)
+(setq-default web-mode-css-indent-offset 2)
 
 (provide 'setup-web-mode)
