2e92fae363b6f60fa963301f3e855e55b7335e49
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2018-11-29 14:22:29 +0000

    Remove explicit setting of rubocop executable

--- config/setup-osx-specifics.el
+++ config/setup-osx-specifics.el
@@ -8,8 +8,5 @@
 ;; Fix path for globally installed utilities
 (setq exec-path-from-shell-check-startup-files nil)
 (exec-path-from-shell-initialize)
-;; Use globally intsalled rubocop, because the above doesn't play well with
-;; chruby
-(setq flycheck-ruby-rubocop-executable "/usr/local/bin/rubocop")
 
 (provide 'setup-osx-specifics)
