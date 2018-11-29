33c5b792b151e11c0a7eb27ca288da2250bc9b33
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2018-11-29 14:22:45 +0000

    Remove mapping from scss-files to css-mode

--- config/setup-file-mappings.el
+++ config/setup-file-mappings.el
@@ -1,7 +1,5 @@
 ;; Javascript
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-;; SCSS
-(add-to-list 'auto-mode-alist '("\\.scss\\'" . css-mode))
 ;; Vue component files
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 ;; Ruby templates
