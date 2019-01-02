22734f1a581b4e873163b0963715a57888974af5
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2019-01-02 12:13:21 +0000

    Add robe

--- config/setup-ruby-mode.el
+++ config/setup-ruby-mode.el
@@ -1,4 +1,7 @@
 ;; Prevent auto-insertion of encoding comment on save
 (setq-default ruby-insert-encoding-magic-comment nil)
 
+(require 'robe)
+(add-hook 'ruby-mode-hook 'robe-mode)
+
 (provide 'setup-ruby-mode)
