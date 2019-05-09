d5c4e53568a19ccadd2869e49434fd7da97e5a53
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2019-05-09 11:25:39 +0100

    Add commands and bindings for runding rspec on current position/file

--- config/setup-ruby-mode.el
+++ config/setup-ruby-mode.el
@@ -1,7 +1,21 @@
+(require 'ruby-mode)
+
 ;; Prevent auto-insertion of encoding comment on save
 (setq-default ruby-insert-encoding-magic-comment nil)
 
 (require 'robe)
 (add-hook 'ruby-mode-hook 'robe-mode)
+
+(require 'setup-projectile)
+(defun run-rspec-on-current-file ()
+  "Executes bundle exec rspec with the current file name."
+  (interactive)
+  (run-shell-command-with-file-name "bundle exec rspec --no-color"))
+(defun run-rspec-on-current-line ()
+  "Executes bundle exec rspec with the current file name and line."
+  (interactive)
+  (run-shell-command-with-file-name-and-line "bundle exec rspec --no-color"))
+(define-key ruby-mode-map (kbd "C-c C-t") 'run-rspec-on-current-file)
+(define-key ruby-mode-map (kbd "C-c t") 'run-rspec-on-current-line)
 
 (provide 'setup-ruby-mode)
