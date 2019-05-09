3d75f78dac3c7449758a661d10faf0d5f75b5571
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2019-05-09 11:24:54 +0100

    Add commands for executing shell commands in projectile root

--- config/setup-projectile.el
+++ config/setup-projectile.el
@@ -1,9 +1,21 @@
 (require 'projectile)
-(projectile-global-mode)
+(projectile-mode)
 
 (setq projectile-completion-system 'ivy)
 
 (define-key projectile-mode-map (kbd "C-x f") 'projectile-find-file)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
+(defun run-shell-command-with-file-name (command)
+  "Invoke a shell command with the current file."
+  (interactive "sCommand: ")
+  (projectile-with-default-dir
+      (projectile-project-root) (compile
+                                 (concat command " " (buffer-file-name)))))
+(defun run-shell-command-with-file-name-and-line (command)
+  "Invoke a shell command with the current file name and line separated by a colon."
+  (interactive "sCommand: ")
+  (projectile-with-default-dir
+      (projectile-project-root) (compile
+                                 (concat command " " (buffer-file-name) ":" (number-to-string (line-number-at-pos))))))
 (provide 'setup-projectile)
