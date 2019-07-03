6ac75e38364330c48a39b9eec4d62170948249e5
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2019-07-03 10:06:59 +0100

    Add rspec outline command to ruby mode

--- config/setup-ruby-mode.el
+++ config/setup-ruby-mode.el
@@ -12,6 +12,11 @@
   "Executes bundle exec rspec with the current file name and line."
   (interactive)
   (run-shell-command-with-file-name-and-line "bundle exec rspec --no-color"))
+(defun run-rspec-outline-on-current-file ()
+  "Executes bundle exec rspec --dry-run --format documentation with the current file name."
+  (interactive)
+  (run-shell-command-with-file-name "bundle exec rspec --no-color --dry-run --format documentation"))
+
 (define-key ruby-mode-map (kbd "C-c C-t") 'run-rspec-on-current-file)
 (define-key ruby-mode-map (kbd "C-c t") 'run-rspec-on-current-line)
 
