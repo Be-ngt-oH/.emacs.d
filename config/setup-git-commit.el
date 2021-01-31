6219d1969358d538d293a237dfe8691da1f747f9
Bengt-Ove Holländer <Bengt.Ove.Hollaender@gmail.com>
2021-01-31 14:01:53 +0000

    Add git-commit

---
+++ config/setup-git-commit.el
@@ -0,0 +1,12 @@
+(require 'git-commit)
+
+(setq git-commit-setup-hook
+      '(git-commit-setup-changelog-support
+        git-commit-propertize-diff
+        git-commit-turn-on-flyspell
+        bug-reference-mode
+        with-editor-usage-message))
+(setq git-commit-summary-max-length 72)
+(add-hook 'git-commit-mode-hook (lambda () (setq fill-column 72)))
+
+(provide 'setup-git-commit)
--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(pos-tip flycheck flycheck-pos-tip auto-complete projectile ivy expand-region markdown-mode multiple-cursors buffer-move editorconfig exec-path-from-shell inf-ruby ripgrep)))
+   '(git-commit pos-tip flycheck flycheck-pos-tip auto-complete projectile ivy expand-region markdown-mode multiple-cursors buffer-move editorconfig exec-path-from-shell inf-ruby ripgrep)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -76,3 +76,4 @@
 (require 'setup-ruby-mode)
 (require 'setup-sql-mode)
 (require 'setup-github-links)
+(require 'setup-git-commit)
