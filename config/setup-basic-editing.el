ca95934771cd87e0398b2ccd10135c0bf1f116c1
Bengt-Ove Holländer <Bengt.Ove.Hollaender@gmail.com>
2021-01-31 14:18:13 +0000

    Remove key-binding swap for newline and newline-and-indent
    Not required anymore since 24.4 enabled `electric-indent-mode` by
    default.

--- config/setup-basic-editing.el
+++ config/setup-basic-editing.el
@@ -9,22 +9,6 @@
 ;; Overwrite region by typing
 (delete-selection-mode 1)
 
-;; Swap the key-bindings of RET and C-j in prog-mode if they have the default
-;; bindings (newline and newline-and-indent).
-(defun swap-bindings (key1 key2)
-  "Swaps the local bindings of key1 and key2."
-  (let ((kb1 (key-binding key1))
-	(kb2 (key-binding key2)))
-    (local-set-key key1 kb2)
-    (local-set-key key2 kb1)))
-
-(add-hook 'prog-mode-hook
-	  (lambda ()
-	    (let ((kbd_RET (kbd "RET")) (kbd_Cj (kbd "C-j")))
-	      (if (equal '(newline newline-and-indent)
-			 (list (key-binding kbd_RET) (key-binding kbd_Cj)))
-		  (swap-bindings kbd_RET kbd_Cj)))))
-
 ;; Add expand-region command and bind it to M-h (replace mark-paragraph)
 (require 'expand-region)
 (global-set-key (kbd "M-h") 'er/expand-region)
