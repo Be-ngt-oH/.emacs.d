5139d37b86d76b0f5e12da77ce50afb6bda39294
Bengt-Ove Holländer <bengt-ove.hollander@freeagent.com>
2022-03-11 00:25:25 +0000

    Disable TLS1.3 because of Emacs bug#34341
    https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
    
    Workaround is suggested on MELPA page. In theory, this bug is fixed in
    27 but still happening for me and other people.

--- config/setup-osx-specifics.el
+++ config/setup-osx-specifics.el
@@ -4,4 +4,7 @@
       mac-command-modifier 'meta
       select-enable-clipboard t)
 
+;; Fix for https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
 (provide 'setup-osx-specifics)
