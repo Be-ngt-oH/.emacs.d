8f1eaba9413622c9e93d0bbd4e6b454dae27cdce
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2022-05-05 15:35:00 +0100

    Add marginalia

--- config/setup-vertico.el
+++ config/setup-vertico.el
@@ -7,4 +7,12 @@
       completion-category-defaults nil
       completion-category-overrides '((file (styles partial-completion))))
 
+(require 'marginalia)
+(marginalia-mode)
+(define-key minibuffer-local-map (kbd "M-a") 'marginalia-cycle)
+(mapc
+ (lambda (x)
+   (setcdr x (cons 'builtin (remq 'builtin (cdr x)))))
+ marginalia-annotator-registry)
+
 (provide 'setup-vertico)
--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(orderless vertico corfu eglot typescript-mode pyvenv git-commit flycheck projectile expand-region markdown-mode multiple-cursors buffer-move editorconfig exec-path-from-shell inf-ruby ripgrep)))
+   '(marginalia orderless vertico corfu eglot typescript-mode pyvenv git-commit flycheck projectile expand-region markdown-mode multiple-cursors buffer-move editorconfig exec-path-from-shell inf-ruby ripgrep)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
