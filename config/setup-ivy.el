589bbe94a47b6e626dbb0f16b910f7a86ddbef57
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2022-05-05 16:38:35 +0100

    Enable orderless completion style in ivy

--- config/setup-ivy.el
+++ config/setup-ivy.el
@@ -4,4 +4,8 @@
 (setq ivy-use-virtual-buffers t)
 (setq ivy-use-selectable-prompt t)
 
+(require 'orderless)
+(setq ivy-re-builders-alist '((t . orderless-ivy-re-builder)))
+(add-to-list 'ivy-highlight-functions-alist '(orderless-ivy-re-builder . orderless-ivy-highlight))
+
 (provide 'setup-ivy)
