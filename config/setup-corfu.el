493ed74e0516b52601df7aa58c271d7aaa96add3
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2022-05-05 16:14:35 +0100

    Enable orderless completion style for corfu via SPC

--- config/setup-corfu.el
+++ config/setup-corfu.el
@@ -4,4 +4,10 @@
 (setq corfu-auto t)
 (setq corfu-preview-current nil)
 
+(require 'orderless)
+(setq completion-styles '(orderless basic)
+      completion-category-defaults nil
+      completion-category-overrides '((file (styles . (partial-completion)))))
+(define-key corfu-map (kbd "SPC") 'corfu-insert-separator)
+
 (provide 'setup-corfu)
