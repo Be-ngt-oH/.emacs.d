67f614559a350192b7fa2a6f31718beefc50774b
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2022-05-06 13:29:33 +0100

    Remove `exec-path-from-shell-check-startup-files` setting
    See https://github.com/purcell/exec-path-from-shell/commit/ad68d367475e698cee043df3af160d85d5fa29e4

--- init.el
+++ init.el
@@ -23,7 +23,6 @@
 
 ;; Fix path for globally installed utilities
 (require 'exec-path-from-shell)
-(setq exec-path-from-shell-check-startup-files nil)
 (exec-path-from-shell-initialize)
 
 ;; Write backup files to own directory
