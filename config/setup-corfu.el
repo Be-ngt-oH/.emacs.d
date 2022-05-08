7d9dd56c926970f6d69ef30e7830b34147ce4c4f
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2022-05-08 14:40:00 +0100

    Enable corfu in minibuffer

--- config/setup-corfu.el
+++ config/setup-corfu.el
@@ -10,4 +10,11 @@
       completion-category-overrides '((file (styles . (partial-completion)))))
 (define-key corfu-map (kbd "SPC") 'corfu-insert-separator)
 
+(defun corfu-enable-in-minibuffer ()
+  "Enable Corfu in the minibuffer if `completion-at-point' is bound."
+  (when (where-is-internal #'completion-at-point (list (current-local-map)))
+    ;; (setq-local corfu-auto nil) Enable/disable auto completion
+    (corfu-mode 1)))
+(add-hook 'minibuffer-setup-hook #'corfu-enable-in-minibuffer)
+
 (provide 'setup-corfu)
