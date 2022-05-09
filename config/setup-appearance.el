f19c5e04c53592596969636e1d92a3e87627204b
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2022-05-09 15:42:28 +0100

    Tweak eglot apperance
    Align with flymake when it comes to unused symbols and also disable
    symbol highlighting.

--- config/setup-appearance.el
+++ config/setup-appearance.el
@@ -20,6 +20,10 @@
 (set-face-attribute 'diff-refine-removed nil :inherit 'diff-removed)
 (set-face-background 'diff-refine-removed nil)
 
+(require 'eglot)
+(set-face-attribute 'eglot-highlight-symbol-face nil :inherit nil)
+(set-face-attribute 'eglot-diagnostic-tag-unnecessary-face nil :inherit 'flymake-note)
+
 (require 'popup)
 (set-face-background 'popup-tip-face "#191a1b")
 (set-face-foreground 'popup-tip-face "white")
