872dffec821fae923ea4cd747416403187c6b05b
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2023-04-04 13:05:43 +0100

    Add prettier

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(flycheck-posframe add-node-modules-path ivy-prescient orderless corfu eglot typescript-mode pyvenv git-commit flycheck projectile ivy expand-region markdown-mode multiple-cursors buffer-move editorconfig exec-path-from-shell inf-ruby ripgrep tree-sitter tree-sitter-langs)))
+   '(flycheck-posframe add-node-modules-path ivy-prescient orderless corfu eglot typescript-mode pyvenv git-commit flycheck projectile ivy expand-region markdown-mode multiple-cursors buffer-move editorconfig exec-path-from-shell inf-ruby ripgrep tree-sitter tree-sitter-langs prettier)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
