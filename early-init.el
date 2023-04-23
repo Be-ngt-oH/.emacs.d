d088cd0b87e3ff39271766ca28c4ffd9617052a5
Bengt-Ove Holländer <bengt.ove.hollaender@gmail.com>
2023-04-23 14:51:35 +0100

    Disable GC during startup
    This saves about 0.5s in my tests.

--- early-init.el
+++ early-init.el
@@ -1,3 +1,8 @@
+;; Disable GC during startup
+(defvar gc-cons-threshold-default gc-cons-threshold)
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold gc-cons-threshold-default)))
+
 ;; Quick way of disabling menu and tool-bar
 ;; https://github.com/doomemacs/doomemacs/blob/master/lisp/doom-start.el
 (push '(menu-bar-lines . 0) default-frame-alist)
